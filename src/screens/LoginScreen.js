3f3bb736790a5f6eac21fd727d0f06ca9ec64987
unknown <jamtechba@gmail.com>
2023-02-25 17:34:50 +0530

    new change

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,153 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Alert,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import {AppStyles} from '../AppStyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch} from 'react-redux';
+import {login} from '../reducers';
+
+function LoginScreen({navigation}) {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+
+  const onLogin =()=>{
+
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, styles.leftTitle]}>Sign In</Text>
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          placeholder="E-mail or phone number"
+          onChangeText={setEmail}
+          value={email}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          secureTextEntry={true}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.facebookContainer, { marginTop: 50 }]}
+        onPress={() => onLogin()}>
+        <Text style={styles.facebookText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      {loading ? (  
+        <ActivityIndicator
+          style={{marginTop: 30}}
+          size="large"
+          animating={loading}
+          color={AppStyles.color.tint}
+        />
+      ) : 
+       null
+      }
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  or: {
+    color: 'black',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: AppStyles.fontSize.title,
+    fontWeight: 'bold',
+    color: AppStyles.color.tint,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  leftTitle: {
+    alignSelf: 'stretch',
+    textAlign: 'left',
+    marginLeft: 20,
+  },
+  content: {
+    paddingLeft: 50,
+    paddingRight: 50,
+    textAlign: 'center',
+    fontSize: AppStyles.fontSize.content,
+    color: AppStyles.color.text,
+  },
+  loginContainer: {
+    alignItems: 'center',
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.tint,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30,
+  },
+  loginText: {
+    color: AppStyles.color.white,
+  },
+  placeholder: {
+    color: 'red',
+  },
+  InputContainer: {
+    width: AppStyles.textInputWidth.main,
+    marginTop: 30,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: AppStyles.color.grey,
+    borderRadius: AppStyles.borderRadius.main,
+  },
+  body: {
+    height: 42,
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: AppStyles.color.text,
+  },
+  facebookContainer: {
+    alignItems: 'center',
+    width: 192,
+    backgroundColor: AppStyles.color.facebook,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30,
+  },
+  facebookText: {
+    color: AppStyles.color.white,
+  },
+  googleContainer: {
+    width: 192,
+    height: 48,
+    marginTop: 30,
+  },
+  googleText: {
+    color: AppStyles.color.white,
+  },
+});
+
+export default LoginScreen;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,103 @@
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+import FastImage from 'react-native-fast-image';
+// import auth from '@react-native-firebase/auth';
+// import firebase from '@react-native-firebase/app';
+// import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch} from 'react-redux';
+import {login} from '../reducers';
+import {AppStyles} from '../AppStyles';
+
+function WelcomeScreen({navigation}) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  
+  // }, []);
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Say hello to your new app</Text>
+      <FastImage
+        style={{ width: 200, height: 200 }}
+        source={{
+          uri: 'https://img1.wsimg.com/isteam/ip/e5efb5b2-e0c9-49e4-8d56-56ef04be68f7/Orange-Moon-Student-Safety-Services.png/:/rs=w:311,h:200,cg:true,m/cr=w:311,h:200/qt=q:95',
+        }}
+      />
+      <TouchableOpacity
+        style={styles.loginContainer}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.loginText}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.signupContainer}
+        onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.signupText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 150,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  title: {
+    fontSize: AppStyles.fontSize.title,
+    fontWeight: 'bold',
+    color: AppStyles.color.tint,
+    marginTop: 20,
+    textAlign: 'center',
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  loginContainer: {
+    alignItems: 'center',
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.tint,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30,
+  },
+  loginText: {
+    color: AppStyles.color.white,
+  },
+  signupContainer: {
+    alignItems: 'center',
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.white,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: AppStyles.color.tint,
+    marginTop: 15,
+  },
+  signupText: {
+    color: AppStyles.color.tint,
+  },
+  spinner: {
+    marginTop: 200,
+  },
+});
+
+export default WelcomeScreen;
