ff64b6282c6cbc62ca13f8684fb66266274e79ab
jamtechdev <jamtechdev@gmail.com>
2023-08-04 02:28:55 +0530

    update heere

--- src/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { combineReducers } from 'redux';
-import authReducer from './reducers/authReducer';
-
-const rootReducer = combineReducers({
-    auth: authReducer,
-});
-
-export default rootReducer;
