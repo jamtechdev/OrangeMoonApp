9c44707da46f209e786bf1703fd2b7ddd2fe0855
JAM-E-145 <jamtechba@gmail.com>
2023-09-28 13:04:19 +0530

    new change

--- App.tsx
+++ App.tsx
@@ -5,14 +5,13 @@ import { store, persistor } from './src/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { PaperProvider } from 'react-native-paper';
-import theme from './src/utils/_css/theme';
 
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <PaperProvider theme={theme}>
+      <PaperProvider>
               <AppNavigator />
       </PaperProvider>
     </PersistGate>
