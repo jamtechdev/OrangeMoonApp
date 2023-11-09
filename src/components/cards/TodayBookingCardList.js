63883507807ec38684833d4cd275f05994c13808
Asim Khan <asim02315@gmail.com>
2023-11-09 11:24:26 +0530

    changeMoment

--- src/components/cards/TodayBookingCardList.js
+++ src/components/cards/TodayBookingCardList.js
@@ -1,38 +1,40 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {ScrollView, StyleSheet, View, Pressable} from 'react-native'; // Import View
-import {Avatar, Button, Card, Text} from 'react-native-paper';
+import { ScrollView, StyleSheet, View, Pressable } from 'react-native'; // Import View
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {formatDate, formatTime} from '../../utils/_helpers';
+import { formatDate, formatTime } from '../../utils/_helpers';
 import globalStyles from '../../utils/_css/globalStyle';
-import {AppStyles} from '../../utils/AppStyles';
-const TodayBookingCardList = ({item, openActionDialog, openPrecheckDialog , navigation, ArrivedMarked}) => {
+import { AppStyles } from '../../utils/AppStyles';
+import moment from 'moment';
+const TodayBookingCardList = ({ item, openActionDialog, openPrecheckDialog, navigation, ArrivedMarked }) => {
   const checkActive = item => {
     const currentDateTime = new Date();
     const firstTime = new Date(currentDateTime);
     const secondTime = new Date(`${item.booking_day.date}T${item.booking_day.start_time}`);
     const interval = Math.abs(secondTime - firstTime) / 36e5; // Calculate the interval in hours
-  
+
     let active = false;
-  
+
     if (interval <= 8 && interval > 1) {
       active = true;
     } else if (interval <= 1 && item.temperature_button === 1) {
       active = true;
     }
-  
+
     // Assuming getBookingTimeZone is already a valid time zone identifier
     const timeZone = item?.booking_day?.booking?.hotel?.state?.time_zone || 'EST';
     const timeOffset = new Date().toLocaleString('en-US', { timeZone, timeZoneName: 'short' });
-    const current_date = new Date(timeOffset).toISOString().split('T')[0];
+
+    const current_date = moment(timeOffset.toString().split(',')[0], 'M/D/YYYY').format('YYYY-MM-DD')
     return current_date;
   };
   const detailHandler = (item) => {
     console.log("item", item)
     const data = new Object({
       id: item?.monitor_booking_day_report?.id,
-      start_Time : item.booking_day.start_time,
-      end_Time : item.booking_day.end_time,
+      start_Time: item.booking_day.start_time,
+      end_Time: item.booking_day.end_time,
     })
     navigation.navigate('DetailsReport', { Booking: data, header: 'today' });
   }
@@ -40,7 +42,7 @@ const TodayBookingCardList = ({item, openActionDialog, openPrecheckDialog , navi
   return (
     <Card style={styles.tableCard}>
       <Card.Content>
-        <View style={[globalStyles.buttonRow, {alignItems: 'center'}]}>
+        <View style={[globalStyles.buttonRow, { alignItems: 'center' }]}>
           <View style={styles.viewRow}>
             <View style={styles.badgeheadingText}>
               <Text style={styles.valueTextinner}>
@@ -58,30 +60,30 @@ const TodayBookingCardList = ({item, openActionDialog, openPrecheckDialog , navi
           </View>
         </View>
 
-        <View style={[styles.viewRow, {flexDirection: 'column', gap: 5}]}>
+        <View style={[styles.viewRow, { flexDirection: 'column', gap: 5 }]}>
           <Text style={styles.keyText}>Group Name </Text>
           <Text style={styles.valueText}>
             {item?.booking_day?.booking?.group_name}
           </Text>
         </View>
-        <View style={[globalStyles.buttonRow, {alignItems: 'center'}]}>
-        <View style={[styles.viewRow, {flexDirection: 'column', gap: 5}]}>
+        <View style={[globalStyles.buttonRow, { alignItems: 'center' }]}>
+          <View style={[styles.viewRow, { flexDirection: 'column', gap: 5 }]}>
             <Text style={styles.keyText}>Start Time</Text>
-              <Text style={styles.valueText}>
-                {formatTime(item?.booking_day?.start_time)}
-              </Text>
+            <Text style={styles.valueText}>
+              {formatTime(item?.booking_day?.start_time)}
+            </Text>
           </View>
-          <View style={[styles.viewRow, {flexDirection: 'column', gap: 5, justifyContent:'flex-end', alignItems:'flex-end'}]}>
+          <View style={[styles.viewRow, { flexDirection: 'column', gap: 5, justifyContent: 'flex-end', alignItems: 'flex-end' }]}>
             <Text style={styles.keyText}>End Time</Text>
-              <Text style={styles.valueText}>
-                {formatTime(item?.booking_day?.end_time)}
-              </Text>
+            <Text style={styles.valueText}>
+              {formatTime(item?.booking_day?.end_time)}
+            </Text>
           </View>
         </View>
 
-        <View style={[globalStyles.buttonRow, {marginTop: 10}]}>
+        <View style={[globalStyles.buttonRow, { marginTop: 10 }]}>
           <View style={styles.action}>
-     
+
             {!item?.monitor_booking_day_report &&
               item?.booking_day?.date <= checkActive(item) && (
                 <Icon
@@ -93,23 +95,23 @@ const TodayBookingCardList = ({item, openActionDialog, openPrecheckDialog , navi
               )}
             {item?.monitor_booking_day_report && (
               <>
-              {item?.arrival_time !== null && (
-                      <Pressable style={styles.badgeButtonDisable} onPress={() => console.log('gsys')}>
-                          <Text style={styles.badgeButtonText}>Arrived</Text>
-                    </Pressable>
-                    ) }
-                    {item?.arrival_time == null && (
-                      <Pressable style={styles.badgeButton} onPress={() => ArrivedMarked(item)}>
-                          <Text style={styles.badgeButtonText}> Mark Arrived</Text>
-                    </Pressable>
-                    ) }
-              <Icon
-                name="eye"
-                onPress={() => detailHandler(item)}
-                size={20}
-                color={AppStyles.color.tint}
-              />
-               </>
+                {item?.arrival_time !== null && (
+                  <Pressable style={styles.badgeButtonDisable} onPress={() => console.log('gsys')}>
+                    <Text style={styles.badgeButtonText}>Arrived</Text>
+                  </Pressable>
+                )}
+                {item?.arrival_time == null && (
+                  <Pressable style={styles.badgeButton} onPress={() => ArrivedMarked(item)}>
+                    <Text style={styles.badgeButtonText}> Mark Arrived</Text>
+                  </Pressable>
+                )}
+                <Icon
+                  name="eye"
+                  onPress={() => detailHandler(item)}
+                  size={20}
+                  color={AppStyles.color.tint}
+                />
+              </>
             )}
             {item?.monitor_booking_day_report && item?.precheckCount === 0 && item?.arrival_time !== null && (
               <Icon
@@ -121,20 +123,20 @@ const TodayBookingCardList = ({item, openActionDialog, openPrecheckDialog , navi
             )}
             {item?.monitor_booking_day_report && item?.precheckCount > 0 && (
               <>
-              <Icon
-                name="plus"
-                // onPress={() => openActionDialog(item)}
-                onPress={() => openPrecheckDialog(item,"plus")}
-                size={20}
-                color={AppStyles.color.tint}
-              />
-               <Icon
-                name="plus-circle"
-                // onPress={() => openActionDialog(item)}
-                onPress={() => openPrecheckDialog(item, "plus-circle" )}
-                size={20}
-                color={AppStyles.color.tint}
-              />
+                <Icon
+                  name="plus"
+                  // onPress={() => openActionDialog(item)}
+                  onPress={() => openPrecheckDialog(item, "plus")}
+                  size={20}
+                  color={AppStyles.color.tint}
+                />
+                <Icon
+                  name="plus-circle"
+                  // onPress={() => openActionDialog(item)}
+                  onPress={() => openPrecheckDialog(item, "plus-circle")}
+                  size={20}
+                  color={AppStyles.color.tint}
+                />
               </>
             )}
             {item?.monitor_booking_day_report &&
@@ -203,30 +205,30 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
-   
+
   },
   valueTextinner: {
     fontSize: 10,
     color: '#fff',
   },
-  badgeButton:{
+  badgeButton: {
     backgroundColor: 'green',
     paddingVertical: 5,
     paddingHorizontal: 15,
-    fontSize:15,
+    fontSize: 15,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  badgeButtonDisable:{
+  badgeButtonDisable: {
     backgroundColor: AppStyles.color.tint,
     paddingVertical: 5,
     paddingHorizontal: 15,
-    fontSize:15,
+    fontSize: 15,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    opacity:0.7
+    opacity: 0.7
   },
   badgeButtonText: {
     fontSize: 14,
