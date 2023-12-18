56704eed6a1a1efa3b8d3fe9d4c85e21f2322490
Jamtechdev <jamtechba@gmail.com>
2023-12-18 19:17:16 +0530

    prod url change

--- src/utils/Connection.js
+++ src/utils/Connection.js
@@ -2,11 +2,11 @@
 
 // #staging urls
 
-export const API_URL = 'https://staging.orangemoonsss.com/api/v1';
-export const IMAGE_URL = 'https://staging.orangemoonsss.com/images/';
+// export const API_URL = 'https://staging.orangemoonsss.com/api/v1';
+// export const IMAGE_URL = 'https://staging.orangemoonsss.com/images/';
 
 
 //# Production urls
 
-// export const API_URL = 'https://app.orangemoonsss.com/api/v1';
-// export const IMAGE_URL = 'https://app.orangemoonsss.com/images/';
+export const API_URL = 'https://app.orangemoonsss.com/api/v1';
+export const IMAGE_URL = 'https://app.orangemoonsss.com/images/';
